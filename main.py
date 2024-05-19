622a67bc3eca00c83f58dc2b2ce0af7e745dcf1a
AlLixAI <alexeylitvinenko@yandex.ru>
2024-05-19 04:15:51 +0300

    rightcommit

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+print_hi('lol')
+
 
 def print_hi(name):
     print(f'Hi, {name}')
+
+
+print_hi('lol')
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+print_hi('lol')
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+print_hi('lol')
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
 
 print_hi('lol')
